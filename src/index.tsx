6d5c6ad39971ff34db0c4ed04a081cd0e4389975
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-30 15:25:37 +0800

    feat: wrap App component in React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./app";
@@ -9,4 +10,8 @@ if (!container) {
 }
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
